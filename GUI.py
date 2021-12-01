eb2651ac9e4b6dffc61aebc8756a057f45005263
Daniel Ryan <ryandaniel625@gmail.com>
2021-12-01 18:32:08 +0000

    Added doc id input box and label

--- GUI.py
+++ GUI.py
@@ -17,25 +17,35 @@ window = tk.Tk()
 
 window.geometry("800x800")
 
+#Document ID Input Label
+input_doc_id_label = tk.Label(window, text="Input doc_id", font="Arial", height=2, width=10)
+input_doc_id_label.place(x=0, y=0)
+
+
+#Document ID Input Box
+input_doc_id = tk.Entry(window) 
+input_doc_id.place(x=0, y=50)
+
+
 #By country button
 by_country_text = tk.StringVar()
 by_country_btn = tk.Button(window, textvariable=by_country_text, font="Arial", bg="Blue", fg="White", height=2, width=10, command=lambda: by_country_plot(doc_id))
 by_country_text.set("By Country")
-by_country_btn.place(x=0, y=0)
+by_country_btn.place(x=0, y=100)
 
 
 #By continent button
 by_continent_text = tk.StringVar()
 by_continent_btn = tk.Button(window, textvariable=by_continent_text, font="Arial", bg="Blue", fg="White", height=2, width=10, command=lambda: by_continent_plot())
 by_continent_text.set("By Continent")
-by_continent_btn.place(x=0, y=50)
+by_continent_btn.place(x=0, y=150)
 
 
 #By browser button
 by_browser_text = tk.StringVar()
 by_browser_btn = tk.Button(window, textvariable=by_browser_text, font="Arial", bg="Blue", fg="White", height=2, width=10, command=lambda : by_browser_plot())
 by_browser_text.set("By Browser")
-by_browser_btn.place(x=0, y=100)
+by_browser_btn.place(x=0, y=200)
 
 toolbarFrame = tk.Frame(window)
 toolbarFrame.place(x=150, y=0)
