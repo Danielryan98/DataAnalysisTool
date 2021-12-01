affdbfb5e61faf78aab0c060f35ef0f763952c8d
Ryan, Daniel F <dfr1@hw.ac.uk>
2021-12-01 09:53:44 +0000

    Update GUI.py, Views.py

---
+++ GUI.py
@@ -0,0 +1,9 @@
+import json
+import tkinter as tk
+import matplot as mp
+
+#Start of window
+window = tk.Tk()
+
+window.mainloop()
+#End of window
---
+++ Views.py
@@ -0,0 +1,13 @@
+import json
+import tkinter as tk
+import matplot as mp
+
+class Views:
+    def bycountry(self):
+        print("Views by ... class")
+
+    def bycontinent(self):
+        print("Views by ... class")
+
+    def bybrowser(self):
+        print("Views by ... class")
