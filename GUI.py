b7ee3d37188ed1b1dab4c6ea5eef33076bdbdcce
Daniel Ryan <ryandaniel625@gmail.com>
2021-12-03 01:07:46 +0000

    UI Changes

--- GUI.py
+++ GUI.py
@@ -6,77 +6,97 @@ import matplotlib.image as mpimg
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
 NavigationToolbar2Tk)
-
+from PIL import ImageTk, Image
 from Views import Views
 
 #### REMBER TO CHANGE THE DOC ID #####
 doc_id = "100713205147-2ee05a98f1794324952eea5ca678c026"
-doc_id1 = "140310170010-0000000067dc80801f1df696ae52862b" #view.sortFunc
+doc_id1 = "140310170010-0000000067dc80801f1df696ae52862b"
 
-visUUID = ""
-docUUID = "140310171202-000000002e5a8ff1f577548fec708d50"
+vis_UUID = ""
+doc_UUID = "140310171202-000000002e5a8ff1f577548fec708d50"
 
-#Start of window
+#Start of window.
 window = tk.Tk()
 
-window.geometry("1200x800")
+window.config(bg="white")
 
-#Document ID Input Label
-input_doc_id_label = tk.Label(window, text="Input doc_id", font="Arial", height=2, width=10)
-input_doc_id_label.place(x=0, y=0)
+button_theme = '#1f77b4'
 
+window.geometry("1100x650")
 
-#Document ID Input Box
-input_doc_id = tk.Entry(window) 
-input_doc_id.place(x=0, y=50)
+#Document ID Input Label.
+document_uuid_label = tk.Label(window, text="document_uuid", font="Arial", height=2, width=12, bg="white")
+document_uuid_label.place(x=30, y=20)
 
+#Document ID Input Box.
+document_uuid = tk.Entry(window, width=75, borderwidth=2) 
+document_uuid.place(x=150, y=32)
 
-#By country button
-by_country_text = tk.StringVar()
-by_country_btn = tk.Button(window, textvariable=by_country_text, font="Arial", bg="Blue", fg="White", height=2, width=15, command=lambda: by_country_plot(doc_id))
-by_country_text.set("By Country")
-by_country_btn.place(x=0, y=100)
+#User ID Input Label.
+visitor_uuid_label = tk.Label(window, text="visitor_uuid", font="Arial", height=2, width=12, bg="white")
+visitor_uuid_label.place(x=45, y=51)
 
-
-#By continent button
-by_continent_text = tk.StringVar()
-by_continent_btn = tk.Button(window, textvariable=by_continent_text, font="Arial", bg="Blue", fg="White", height=2, width=15, command=lambda: by_continent_plot(doc_id))
-by_continent_text.set("By Continent")
-by_continent_btn.place(x=0, y=150)
+#User ID Input Box.
+visitor_uuid = tk.Entry(window, width=75, borderwidth=2) 
+visitor_uuid.place(x=150, y=63)
 
 
-#By browser button
-by_browser_text = tk.StringVar()
-by_browser_btn = tk.Button(window, textvariable=by_browser_text, font="Arial", bg="Blue", fg="White", height=2, width=15, command=lambda : by_browser_plot())
-by_browser_text.set("By Browser")
-by_browser_btn.place(x=0, y=200)
+def paint_logo():
+    global img
+    path = "IP_LOGO.jpg"
+    img= (Image.open(path))
+    img = img.resize((425,100), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    panel = tk.Label(window, image=img, height=100, width=425, bd=0)
+    panel.place(x=625, y=0)
 
-#Also likes button
-also_likes_text = tk.StringVar()
-also_likes_btn = tk.Button(window, textvariable=also_likes_text, font="Arial", bg="Blue", fg="White", height=2, width=15, command=lambda : determine_func())
-also_likes_text.set("Also Likes")
-also_likes_btn.place(x=0, y=250)
+paint_logo()
 
+#Toolbar frame to hold toolbar.
 toolbarFrame = tk.Frame(window)
-toolbarFrame.place(x=150, y=0)
+toolbarFrame.config(bg="white")
+toolbarFrame.place(x=535, y=600)
 
-#Also likes graph button
-also_likes_graph_text = tk.StringVar()
-also_likes_graph_btn = tk.Button(window, textvariable=also_likes_graph_text, font="Arial", bg="Blue", fg="White", height=2, width=15, command=lambda : determine_func())
-also_likes_graph_text.set("Also Likes Graph")
-also_likes_graph_btn.place(x=0, y=300)
-
-#Walkthrough button
-tutorial_text = tk.StringVar()
-tutorial_btn = tk.Button(window, textvariable=tutorial_text, font="Arial", bg="Blue", fg="White", height=2, width=15, command=lambda : determine_func())
-tutorial_text.set("Tutorial")
-tutorial_btn.place(x=0, y=350)
-
-toolbarFrame = tk.Frame(window)
-toolbarFrame.place(x=150, y=0)
-
+#Graph frame to hold the graph.
 graphFrame = tk.Frame(window)
-graphFrame.place(x=150, y=50)
+graphFrame.place(x=155, y=100)
+
+#By country button.
+by_country_text = tk.StringVar()
+by_country_btn = tk.Button(window, textvariable=by_country_text, font="Arial", bg=button_theme, fg="White", borderwidth=5, highlightbackground="black", highlightthickness=2, height=2, width=15, command=lambda: by_country_plot(doc_id))
+by_country_text.set("By Country")
+by_country_btn.place(x=45, y=160)
+
+#By continent button.
+by_continent_text = tk.StringVar()
+by_continent_btn = tk.Button(window, textvariable=by_continent_text, font="Arial", bg=button_theme, fg="White", borderwidth=5, highlightbackground="black", highlightthickness=2, height=2, width=15, command=lambda: by_continent_plot(doc_id))
+by_continent_text.set("By Continent")
+by_continent_btn.place(x=45, y=230)
+
+#By browser button.
+by_browser_text = tk.StringVar()
+by_browser_btn = tk.Button(window, textvariable=by_browser_text, font="Arial", bg=button_theme, fg="White", borderwidth=5, highlightbackground="black", highlightthickness=2, height=2, width=15, command=lambda : by_browser_plot())
+by_browser_text.set("By Browser")
+by_browser_btn.place(x=45, y=300)
+
+#Also likes button.
+also_likes_text = tk.StringVar()
+also_likes_btn = tk.Button(window, textvariable=also_likes_text, font="Arial", bg=button_theme, fg="White", borderwidth=5, highlightbackground="black", highlightthickness=2, height=2, width=15, command=lambda : determine_func())
+also_likes_text.set("Also Likes")
+also_likes_btn.place(x=45, y=370)
+
+#Also likes graph button.
+also_likes_graph_text = tk.StringVar()
+also_likes_graph_btn = tk.Button(window, textvariable=also_likes_graph_text, font="Arial", bg=button_theme, fg="White", borderwidth=5, highlightbackground="black", highlightthickness=2, height=2, width=15, command=lambda : determine_func())
+also_likes_graph_text.set("Also Likes Graph")
+also_likes_graph_btn.place(x=45, y=440)
+
+#Walkthrough button.
+tutorial_text = tk.StringVar()
+tutorial_btn = tk.Button(window, textvariable=tutorial_text, font="Arial", bg=button_theme, fg="White", borderwidth=5, highlightbackground="black", highlightthickness=2, height=2, width=15, command=lambda : determine_func())
+tutorial_text.set("Tutorial")
+tutorial_btn.place(x=45, y=510)
 
 
 
@@ -99,10 +119,6 @@ def plot(browser_dict):
 
     clear_widgets()
 
-   # the figure that will contain the plot
-    fig = Figure(figsize = (10, 5),
-            dpi = 100)
-
     print(browser_dict.items())
 
     x_items = []
@@ -115,41 +131,37 @@ def plot(browser_dict):
     x = x_items
     y = y_items
 
-    # adding the subplot
+    #Figure that will contain the plot.
+    fig = Figure(figsize = (10, 5),
+            dpi = 100)
+
+    # Create the subplot. Set x & y and bar thickness. Plot the graph.
     plot1 = fig.add_subplot(111)
-
-    #Set x & y and bar thickness
     plot1.bar(x, y, .5)
-
-    # plotting the graph
     plot1.plot()
 
-    # creating the Tkinter canvas
-    # containing the Matplotlib figure
+    # creating the Tkinter canvas containing the Matplotlib figure,
+    # and placing the canvas on the Tkinter window.
     canvas = FigureCanvasTkAgg(fig, master = graphFrame)  
     canvas.draw()
-
-    # placing the canvas on the Tkinter window
     canvas.get_tk_widget().pack()
 
-
-    # creating the Matplotlib toolbar
+    # creating the Matplotlib toolbar and placing it on the Tkinter window.
     toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
+    toolbar.config(bg="white")
     toolbar.update()
-
-    # placing the toolbar on the Tkinter window
     toolbar.pack()
 
 def clear_widgets():
-    #Reset toolbar frame to allow for new graph toolbar
+    #Reset toolbar frame to allow for new graph toolbar.
     for widget in toolbarFrame.winfo_children():
         widget.destroy()
 
-    #Reset chart frame to allow for new graph
+    #Reset chart frame to allow for new graph.
     for widget in graphFrame.winfo_children():
         widget.destroy()
 
-#Function taken from https://stackoverflow.com/a/45846841 user 'rtaft'
+#Function taken from https://stackoverflow.com/a/45846841 user 'rtaft'.
 def format_number(num):
     num = float('{:.3g}'.format(num))
     magnitude = 0
@@ -158,26 +170,37 @@ def format_number(num):
         num /= 1000.0
     return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
 
+def make_list(xs_sort):
+    print(xs_sort)
 
 def make_graph(xs_sort):
 
-    instance = Views()
-    data_size = format_number(instance.dataList.__len__())
-
+    #Clear the canvas.
     clear_widgets()
 
-    main_user_id = visUUID[-4:]
-    main_doc_id = docUUID[-4:]
+    #Views instance to grab data from.
+    views = Views()
+
+    #Format the key data so that it's in format 300k, 1m, 3m etc.
+    data_size = format_number(views.dataList.__len__())
+
+    #Slice the id's so that we only work with the last 4 characters. 
+    main_user_id = vis_UUID[-4:]
+    main_doc_id = doc_UUID[-4:]
+
+    #Node Shapes
+    user_shape = 'box'
+    has_read_shape = 'circle'
 
     # the figure that will contain the plot
     fig = Figure(figsize = (10, 5),
             dpi = 100)
+    
 
-    user_shape = 'box'
-    has_read_shape = 'circle'
-
+    #New graphviz digraph called also likes
     dot = gv.Digraph('also likes')
 
+    #Graph key
     dot.node('Readers', 'Readers', shape='none')
     dot.node('Documents', 'Documents', shape='none')
     dot.edge('Readers', 'Documents', label='Size: ' + data_size)
@@ -223,17 +246,12 @@ def make_graph(xs_sort):
 
 def determine_func():
 
-    
+    views = Views()
 
-    instance = Views()
-
-    if docUUID and visUUID:      
-        xs_sort = instance.alsoLikes(docUUID, visUUID, instance.sortFunc)
-        make_graph(xs_sort)
+    if doc_UUID and vis_UUID:      
+        make_graph(views.alsoLikes(doc_UUID, vis_UUID, views.sortFunc))
     else:
-        xs_sort = instance.alsoLikes(docUUID, instance.sortFunc)
-        make_graph(xs_sort)
-
+        make_graph(views.alsoLikes(doc_UUID, views.sortFunc))
 
 
 window.mainloop()
